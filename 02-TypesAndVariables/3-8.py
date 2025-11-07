ab61c83f2c81523957252cf7bf417da95852f5fe
Maciej Bentkowski <m.bentkowski04@gmail.com>
2025-11-07 12:49:41 +0100

    zadania z sekcji 1 i 2

--- 02-TypesAndVariables/3-8.py
+++ 02-TypesAndVariables/3-8.py
@@ -5,6 +5,6 @@
 math = 5
 art = 4
 music = 5
-history = '3'
-average = mat + Art - muzic + history / 4
-print("Average grade is', averege)
+history = 5
+average = (math + art + music + history) / 4
+print("Average grade is", average)
---
+++ 03-ControlStructures/2-6.py
@@ -0,0 +1,10 @@
+number = int(input('Number is: '))
+
+if number > 0:
+    print("number is positive")
+elif number < 0:
+    print("number is negative")
+elif number == 0:
+    print("number is 0")
+
+
